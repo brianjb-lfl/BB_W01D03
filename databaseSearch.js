fa4721e8dcb024581066ce62a7e3aa73d8cccfba
bradcgarner <brad@bradgarner.com>
2017-08-23 21:11:13 -0400

    updated databaseSearch.js

--- databaseSearch.js
+++ databaseSearch.js
@@ -12,21 +12,18 @@ const HEROES = [
 
 function findOne(arr, query) {
   let itemGood;
-  // loop thru array
-  for (let x = 0; x < arr.length; x++) {
+  for (let x = 0; x < arr.length; x++) { // loop thru array
     //console.log(`new ${x} loop`);
     itemGood = true;
-    // loop thru properties
-    for (let i = 0; i <= Object.keys(query).length; i++) {
+    for (let i = 0; i <= Object.keys(query).length; i++) { // loop thru properties
       //console.log(`new ${i} loop`);
       let theKey = Object.keys(query)[i];
       //console.log(theKey);
-      if (arr[x][theKey] !== query[theKey]) {
-        //console.log('I broke');
-        itemGood = false;        
+      if (arr[x][theKey] !== query[theKey]) { // if the current item.key !== the matching key in query (e.g. arr[1].id !== query.id )
+        itemGood = false;
       } // end if
     } // end inner for loop properties
-    if(itemGood){
+    if (itemGood) {
       //console.log('I am returning');
       return arr[x];
     }
@@ -36,8 +33,20 @@ function findOne(arr, query) {
 }
 
 let firstQuery = findOne(HEROES, { squad: 'Avengers', name: 'Hulk' });
-console.log(firstQuery);
+//console.log(firstQuery);
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~
+function findMatches(arr, query) {
+  let queryKeys = Object.keys(query); // (e.g. ['name', 'squad'])
+  return arr.find(item => { // loop thru array of all heroes
+    let arrKeys = Object.keys(item); //get the keys for this item (e.g. ['name', 'squad'])
+    let keyMatch = arrKeys.filter(key => item[key] === query[key]);
+    if ((keyMatch.length === queryKeys.length) && keyMatch.length > 0) { return true; }
+  }); // end for loop
+}
+
+let secondQuery = findMatches(HEROES, { squad: 'Avengers', id: 3 });
+console.log(secondQuery);
 // item.Object.keys(query)[i] ===  query[Object.keys(query)[i]]
 // sample data
 // [id, name], so 0 = id      ===  query.id === Captain America
